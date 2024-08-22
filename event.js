a8211e9ed0df7357e9456999c39bb1a44cec97fb
ASHISH ADHIKARI <ashish.adhikary.2003@gmail.com>
2024-08-22 10:28:05 +0600

    JS file has been added successfully

---
+++ event.js
@@ -0,0 +1,13 @@
+// option 2 : add onclick function
+
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+//   option 3 :
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
